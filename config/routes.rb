4d204c6bad2f078763d8e923f52ba7c3bb69abfe
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-12 05:41:28 +0000

    [Bookers2] booksコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
