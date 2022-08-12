863041860efcb75ce53c1f31f47200c0fd893fa9
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-12 08:26:07 +0000

    [Bookers2]投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
+  resources :books, only:[:create, :index, :show, :edit]
 end
