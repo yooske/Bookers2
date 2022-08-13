df98a1e26298935ba5edac2e1670d473fa763cba
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-13 02:53:30 +0000

    [Bookers2]投稿の編集機能と削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :books, only:[:create, :index, :show, :edit]
+  resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
 end
