630eb7906e166562428456cc1970d9d13eebe98e
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-16 15:33:06 +0000

    [Bookers2]プロフィール編集へのリンクの作成、部分テンプレートの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,6 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to books_path(current_user.id)
+    redirect_to user_path(current_user.id)
   end
 
   private
