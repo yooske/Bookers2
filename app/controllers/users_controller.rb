db193e3769b37e913b48b5dadfb73508e61bea0d
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-17 09:23:04 +0000

    [bookers2] userページの部分テンプレートの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
 
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
   end
