283032d2d8442182ac4d1e1d0261f2e8b34d8cd5
EC2 Default User <ec2-user@ip-172-31-18-24.ap-northeast-1.compute.internal>
2022-08-31 15:14:50 +0000

    [応用課題6]favoritesコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only:[:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only:[:create, :destroy]
+  end
   resources :users, only:[:index, :show, :edit, :update]
 end
