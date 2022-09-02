4146dd6ceed3b1657c379238ebcbf002f4854b3b
Yousuke <yooske0214@gmail.com>
2022-09-02 09:25:46 +0000

    [応用課題6]コメント機能の非同期通信の調整中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,16 +2,18 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
+    @book_comment = BookComment.new
   end
-  
+
   def destroy
-    BookComment.find(params[:id]).destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:id]).destroy
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
