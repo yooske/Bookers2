cab61a0415822798f353f2674a51c7be724342a5
Yousuke <yooske0214@gmail.com>
2022-09-03 08:18:18 +0000

    [応用課題6]コメント機能の非同期通信の実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,6 +2,7 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
+    @test = current_user.book_comments.new
     @book_comment.book_id = @book.id
     @book_comment.save
   end
