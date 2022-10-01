277dafd408b703f3ca14bcd65c9a2fd77d274859
yooske <yooske0214@gmail.com>
2022-10-01 15:27:16 +0000

    [edit] booksのsort記述を修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,10 @@ class BooksController < ApplicationController
   def index
     to  = Time.current.at_end_of_day
     from  = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).sort {|a,b|b.favorited_users.where(created_at: from...to).size <=> a.favorited_users.where(created_at: from...to).size}
+    @books = Book.all.sort {|a,b|
+    b.favorites.where(created_at: from...to).size <=>
+    a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
   end
 
