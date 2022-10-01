5c5b9a7d0cdca9b431ca2266d3302b13116c3019
yooske <yooske0214@gmail.com>
2022-10-01 15:17:35 +0000

    [add] booksを一週間でいいねが多い順に並べる

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
 
   def index
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).sort {|a,b|b.favorited_users.where(created_at: from...to).size <=> a.favorited_users.where(created_at: from...to).size}
     @book = Book.new
-    @books = Book.all
   end
 
   def create
